8720a358d5dc2379a2ab18f2f8fe8f6ce0640149
advaitdeshmukh <35995289+advaitdeshmukh@users.noreply.github.com>
2026-08-01 21:19:01 +0000

    Deploying to gh-pages from @ advaitdeshmukh/advaitdeshmukh.github.io@983f8bedd2b0aaa466131f418435afc8a1e30292 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -62,7 +62,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/Resume.pdf", "_blank");
+          window.open("/assets/pdf/CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
